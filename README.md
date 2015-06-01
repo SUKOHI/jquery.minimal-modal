aae989c670f09106e9748b7903146d8ff8c675eb
sukohi <eagle.fly.free0722@gmail.com>
2015-06-01 19:55:23 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,87 @@
+jquery.minimal-modal
+=========================
+
+A jQuery package to manage modal very simply.
+(Tested on IE 11, Firefox, Google Chrome)
+
+Requirements
+====
+
+jQuery
+
+Installation
+=====
+Using bower is the simplest way.
+
+    bower install jquery.minimal-modal --save-dev
+
+Usage
+====
+
+**Simple Way 1**
+
+    (in HTML)
+    
+    <a href="#" id="click-button">click-button</a>
+    <div id="other-content" style="display:none;">
+        You can display this.
+        <br><br>
+        <div style="text-align: right">
+            <a href="#" class="minimal-modal-close">Close</a>
+        </div>
+    </div>
+    
+
+    (in JavaScript)
+
+    $.minimalModal({
+        selector: '#click-button',      // Click button selector to open the modal.
+        content: '#other-content'       // You need to prepare content in HTML.
+    });
+    
+
+**Simple Way 2**
+
+    (in HTML)
+    
+    <a href="#" id="click-button">click-button</a>
+    
+    
+    (in JavaScript)
+
+    $.minimalModal({
+        selector: '#click-button',
+        content: function(e){
+
+            return 'Content you want.<button class="minimal-modal-close">close</button>';
+
+        }
+    });
+    
+    
+**with Options**
+
+    $.minimalModal({
+        selector: '#click-button',
+        fadeIn: 'slow',             // Optional(Default: slow)
+        fadeOut: 'slow',            // Optional(Default: slow)
+        width: '80%',               // Optional(Default: 50%) *px is also available.
+        maxHeight: '150px',         // Optional
+        content: function(e){
+
+            return 'Content <button class="minimal-modal-close">close</button>';
+
+        }, 
+        callback: function(e){      // Optional
+
+            console.log('callback');
+
+        }
+    });
+
+License
+====
+
+This package is licensed under the MIT License.
+
+Copyright 2015 Sukohi Kuhoh
---
+++ jquery.minimal-modal.js
@@ -0,0 +1,146 @@
+;(function($) {
+
+    jQuery.minimalModal = function(parameters) {
+
+        var MM = {
+
+            ids: {
+                overlay: 'minimal-modal-overlay',
+                content: 'minimal-modal-content',
+                htmlContent: 'minimal-modal-html-content',
+                close: 'minimal-modal-close'
+            },
+            css: {
+                content: {
+                    width: '50%',
+                    margin: 0,
+                    padding: '10px 20px',
+                    border: '2px solid #aaa',
+                    background: '#fff',
+                    position: 'fixed',
+                    display: 'none',
+                    'z-index': 10002
+                },
+                overlay: {
+                    'z-index': 1,
+                    display: 'none',
+                    position: 'fixed',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '120%',
+                    'background-color': 'rgba(0,0,0, 0.75)'
+                }
+            },
+            originalScrollX: 0,
+            originalScrollY: 0,
+            init: function() {
+
+                $.each(['overlay', 'content'], function(index, key){
+
+                    $('body').append('<div id="'+ MM.ids[key] +'" style="'+ MM.inlineCss(MM.css[key]) +'"></div>');
+
+                });
+                $('#click-button').on('click', function(e){
+
+                    MM.open(e);
+                    return false;
+
+                });
+
+            },
+            open: function(e) {
+
+                MM.originalScrollX = $(window).scrollLeft();
+                MM.originalScrollY = $(window).scrollTop();
+
+                var fadeInSpeed = MM.fadeSpeed('fadeIn');
+                $('#'+ MM.ids.overlay).fadeIn(fadeInSpeed);
+                MM.centerContent();
+
+                var content = '';
+
+                if($.type(parameters['content']) == 'function') {
+
+                    content = parameters['content'](e);
+
+                } else {
+
+                    content = $(parameters['content']).html();
+
+                }
+
+                $('#'+ MM.ids.content).html(content).fadeIn(fadeInSpeed);
+                $('#'+ MM.ids.overlay +', .'+ MM.ids.close).on('click', function(){
+
+                    $(window).scrollTop(MM.originalScrollY);
+                    $(window).scrollLeft(MM.originalScrollX);
+
+                    var fadeOutSpeed = MM.fadeSpeed('fadeOut');
+                    $('#'+ MM.ids.content +', #'+ MM.ids.overlay).fadeOut(fadeOutSpeed);
+                    return false;
+
+                });
+
+                $(window).resize(MM.centerContent())
+
+                if($.type(parameters['callback']) == 'function') {
+
+                    parameters['callback'](e);
+
+                }
+
+            },
+            centerContent: function(){
+
+                var windowWidth = $(window).width();
+                var windowHeight = $(window).height();
+                var outerWidth = $('#'+ MM.ids.content).outerWidth(false);
+                var outerHeight = $('#'+ MM.ids.content).outerHeight(false);
+                var top = Math.max(0, ((windowHeight - outerHeight) / 2) + $(window).scrollTop()) +'px';
+                var left = Math.max(0, ((windowWidth - outerWidth) / 2) + $(window).scrollLeft()) +'px';
+
+                $('#'+ MM.ids.content).css({
+                    top: top,
+                    left: left
+                })
+
+            },
+            fadeSpeed: function(mode) {
+
+                return (parameters[mode] != undefined) ? parameters[mode] : 'slow';
+
+            },
+            inlineCss: function(cssValues) {
+
+                var content = '';
+
+                $.each(cssValues, function(key, value){
+
+                    content += key +':'+ value +';';
+
+                });
+
+                return content;
+
+            }
+
+        };
+
+        if(parameters['width'] != undefined) {
+
+            MM.css.content['width'] = parameters['width'];
+
+        }
+        if(parameters['maxHeight'] != undefined) {
+
+            MM.css.content['overflow'] = 'auto';
+            MM.css.content['max-height'] = parameters['maxHeight'];
+
+        }
+
+        MM.init();
+
+    }
+
+})(jQuery);
